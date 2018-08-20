6900e97807a66f21fa8e8edf8043fd343adfb9c4
Vladas Zakrevskis <146100@gmail.com>
2018-08-20 21:45:47 +0300

    simplified compiler

--- Compiler.py
+++ Compiler.py
@@ -3,24 +3,30 @@ import Shell
 import System
 
 class Compiler(object):
-	def __init__(self, name = '', cppname = '', version = '', libcxx = 'libstdc++'):
+
+	def __init__(self, name = '', cppname = '', version = ''):
 		self.name        = name
 		self.cppname     = cppname
 		self.version     = version
-		self.libcxx      = libcxx
+		self.libcxx      = self._libcxx()
 
 	def isVS(self):
 		return self.name == 'Visual Studio'
 
 	def isApple(self):
 		return self.name == 'apple-clang'
+		
+	def _libcxx(self):
+		return 'libc++' if self.isApple() else 'libstdc++'
+
+
 
 gcc_version = Shell.get(['gcc', '-dumpversion'])[:3]
 
-visualStudio = Compiler('Visual Studio', version = '15'             )
-gcc          = Compiler('gcc',           'g++',      gcc_version    )
-clang        = Compiler('clang',         'clang++',  '6.0'          )
-appleClang   = Compiler('apple-clang',   'clang++',  '9.1', 'libc++')
+visualStudio = Compiler('Visual Studio', version = '15'         )
+gcc          = Compiler('gcc',           'g++',      gcc_version)
+clang        = Compiler('clang',         'clang++',  '6.0'      )
+appleClang   = Compiler('apple-clang',   'clang++',  '9.1'      )
 
 def default():
 	if System.isWindows:
