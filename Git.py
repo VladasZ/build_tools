455a9394895074c6c690519706f9a25cb7bd3f2f
Vladas Zakrevskis <146100@gmail.com>
2018-11-14 16:36:52 +0300

    ide generator fix, git root dir from submodule

--- Git.py
+++ Git.py
@@ -1,10 +1,16 @@
 import File
 import Debug
+import Shell
 
 def root_dir(path = '.'):
     _path = path
     while not File.is_root(_path):
         if File.exists(_path + '/.git'):
+            super_project = Shell.get(["git", "rev-parse", "--show-superproject-working-tree"])
+            if super_project:
+                return super_project
             return File.full_path(_path)
         _path = _path + "/.."
-    Debug.throw("Conan root directory not found for path: " + File.full_path(path))
+    Debug.throw("Git root directory not found for path: " + File.full_path(path))
+
+    
