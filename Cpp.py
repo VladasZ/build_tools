ebcd21f7ae06ca4befb09dc2cd587c24af420b03
Vladas Zakrevskis <146100@gmail.com>
2018-11-16 19:08:21 +0300

    building without conan

--- Cpp.py
+++ Cpp.py
@@ -3,20 +3,15 @@ import File
 import Make
 import Time
 import Args
+import File
 import Shell
 import Debug
 import Conan
 import Cmake
 
-def project_name(path = "."):
-    _path = path
-    while not File.is_root(_path):
-        if File.parent_folder(_path) == "source":
-            return File.folder_name(_path)
-        _path = "../" + _path
-    Debug.throw("Source directory not found for path: " + File.full_path(path))
-
 root_dir = Git.root_dir()
+project_name = File.folder_name(root_dir)
+needs_conan = File.exists(root_dir + "/conanfile.txt")
 build_dir = root_dir + "/build"
 
 stamp = Time.stamp()
@@ -24,13 +19,15 @@ stamp = Time.stamp()
 def prepare():
     File.cd(root_dir)
 
-    Conan.setup()
+    if needs_conan:
+        Conan.setup()
     Cmake.setup()
 
     File.mkdir('build')
     File.cd('build')
 
-    Conan.run()
+    if needs_conan:
+        Conan.run()
     Cmake.run()
     print("Project prepare time: " + Time.duration())
     
@@ -42,9 +39,14 @@ def build():
     print("Project build time: " + Time.duration())
 
 def run():
-    _project_name = "Test" if Args.test else project_name()
+    _project_name = "Test" if Args.test else project_name
     build()
-    Shell.run([File.full_path(build_dir) + "/bin/" + _project_name])
+    bin_dir = File.full_path(build_dir) + "/"
+
+    if File.exists(bin_dir + "bin"):
+        bin_dir += "bin/"
+    
+    Shell.run([bin_dir + _project_name])
 
 def clean():
     File.rm(build_dir)
