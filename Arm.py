85b9dcb89e9b56598624dd6eff373f18036b43a0
Vladas Zakrevskis <146100@gmail.com>
2018-12-21 13:30:50 +0300

    changed arm build

--- Arm.py
+++ Arm.py
@@ -4,12 +4,12 @@ import Shell
 
 def build():
     Shell.run(["make"])
-    Shell.run(["arm-none-eabi-objcopy", "-O", "binary", "NUCLEO_F446RE/arm.elf", "NUCLEO_F446RE/arm.bin"])
+#    Shell.run(["arm-none-eabi-objcopy", "-O", "binary", "NUCLEO_F446RE/arm.elf", "NUCLEO_F446RE/arm.bin"])
 
 def run():
     build()
-    File.copy("NUCLEO_F446RE/arm.bin", "/media/vladas/NODE_F446RE/arm.bin")
+    File.copy("BUILD/Nucleo_blink_led.bin", "/media/vladas/NODE_F446RE/Nucleo_blink_led.bin")
     
     
 def clean():
-    File.rm("NUCLEO_F446RE")
+    File.rm("BUILD")
