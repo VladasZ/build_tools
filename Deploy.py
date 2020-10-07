667959cd3959f01938cbc8672d998cb82f777319
Vladas Zakrevskis <146100@gmail.com>
2020-10-07 23:10:56 +0300

    deploy apps list

---
+++ Deploy.py
@@ -0,0 +1,31 @@
+
+cask_map = {
+    "toolbox" : "jetbrains-toolbox",
+    "teams"   : "microsoft-teams",
+    "vk"      : "vk-messenger"
+}
+
+choco_map = {
+    "toolbox" : "jetbrainstoolbox",
+    "teams"   : "microsoft-teams",
+    "vk"      : "vkmessenger"
+}
+
+win_only = [
+    "putty",
+    "dependencywalker"
+]
+
+apps = [
+    "transmission",
+    "wireshark",
+    "gitkraken"
+    "telegram",
+    "postman",
+    "toolbox",
+    "skype",
+    "slack",
+    "teams"
+    "steam"
+    "vk"
+]
