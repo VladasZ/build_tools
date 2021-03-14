51f78a017c9d5faacb51f351266405eb9dbb760b
Vladas Zakrevskis <146100@gmail.com>
2021-03-14 12:40:37 +0300

    ssh paths

--- Paths.py
+++ Paths.py
@@ -16,8 +16,8 @@ my = dev + "/my"
 
 money = my + "/money"
 
-repo_root = "https://github.com/vladasz/"
-#repo_root = "git@github.com:VladasZ/"
+#repo_root = "https://github.com/vladasz/"
+repo_root = "git@github.com:VladasZ/"
 
 #git@github.com:VladasZ/test_engine.git
 
@@ -49,13 +49,16 @@ my_projects = {
     home + '/dev/my/money/HabitServer' : repo_root + 'HabitServer.git',
 }
 
+#tesla_root = "https://gitlab.vrweartek.com/"
+tesla_root = "git@gitlab.vrweartek.com:"
+
 tesla_projets = {
-    home + '/dev/work/tes/teslasuit-studio'    : 'https://gitlab.vrweartek.com/software/teslasuit-studio.git',
-    home + '/dev/work/tes/glove/GloveCommon'   : 'https://gitlab.vrweartek.com/Research/GloveCommon.git',
-    home + '/dev/work/tes/glove/glovefirmware' : 'https://gitlab.vrweartek.com/Research/glovefirmware.git',
-    home + '/dev/work/tes/glove/GloveSoftware' : 'https://gitlab.vrweartek.com/Research/GloveSoftware.git',
-    home + '/dev/work/tes/glove/GloveUnity'    : 'https://gitlab.vrweartek.com/Research/gloveunity.git',
-    home + '/dev/work/tes/glove/node'          : 'https://gitlab.vrweartek.com/firmware/tglove-node'
+    home + '/dev/work/tes/teslasuit-studio'    : tesla_root + "software/teslasuit-studio.git",
+    home + '/dev/work/tes/glove/GloveCommon'   : tesla_root + "Research/GloveCommon.git",
+    home + '/dev/work/tes/glove/glovefirmware' : tesla_root + "Research/glovefirmware.git",
+    home + '/dev/work/tes/glove/GloveSoftware' : tesla_root + "Research/GloveSoftware.git",
+    home + '/dev/work/tes/glove/GloveUnity'    : tesla_root + "Research/gloveunity.git",
+    home + '/dev/work/tes/glove/node'          : tesla_root + "firmware/tglove-node"
 }
 
 atom_projects = {
